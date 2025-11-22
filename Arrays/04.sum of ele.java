4480467849e340a17043d699e98274d8536ddc4f
savisalanjala17 <savisalanjala17@gmail.com>
2025-11-22 18:53:52 +0530

    Add Java program to sum elements of an array

---
+++ Arrays/04.sum of ele.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int a[] = new int [n];
+		int sum = 0;
+
+		
+		
+		for(int i=0 ; i<a.length ; i++){
+		    
+		    a[i] = sc.nextInt();
+		    sum+=a[i];
+		}
+		
+		System.out.print(sum);
+		
+		
+
+	
+
+	}
+}
